ed1b3b7e851c1b26f4443bc6eeba48832895165c
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-22 12:16:48 +0100

    Update baseController.js

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,1 +1,16 @@
+const utilities = require("../utilities/")
+const baseController = {}
+
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  req.flash("notice", "This is a flash message.")
+  res.render("index", {title: "Home", nav})
+}
+
+// Error link in footer
+baseController.buildError = async function(req, res){
+  res.render("./views/errors/error", {title: "Error", nav})
+}
+
+module.exports = baseController
 
